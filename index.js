9373ac651639605a0498fc56919618704ce0f0c3
Ubuntu <diloo@AppDiloo1.tj3vzrhq45sunm33c1uz3aqolh.bx.internal.cloudapp.net>
2016-07-01 16:31:24 +0000

    i

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var io = require('socket.io')(http);
 var request = require('request');
 
 var port = process.env.PORT || 3000
-var basePath = "http://dilooapp.com" ;
+var basePath = "http://40.114.91.208:1337" ;
 /*
 app.get('/', function(req, res){
   res.sendfile('index.html');
